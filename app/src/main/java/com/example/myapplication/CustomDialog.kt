47bfcbf39e357d739266e729c57dc20eee12c4f0
west <jsistop16@naver.com>
2021-12-28 18:04:58 +0900

    customdialog에서 db객체 생성 시도

--- app/src/main/java/com/example/myapplication/CustomDialog.kt
+++ app/src/main/java/com/example/myapplication/CustomDialog.kt
@@ -2,35 +2,41 @@ package com.example.myapplication
 
 import android.app.Dialog
 import android.content.Context
+import android.os.Bundle
 import android.view.WindowManager
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.room.Room
+import com.example.myapplication.databinding.CustomDialogBinding
 
 
-//다이얼로그 창 클래스
+//다이얼로그 클래스
 class CustomDialog(context : Context) : AppCompatActivity(){
     private val dialog = Dialog(context)
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        val binding : CustomDialogBinding = CustomDialogBinding.inflate(layoutInflater)
+//        val rootview = binding.root
+//        setContentView(rootview)
+        val db = Room.databaseBuilder(
+            applicationContext,
+            UserDatabase::class.java,"database"
+        ).allowMainThreadQueries().build()
+        //db객체 생성
+    }
+
     fun show(){
         dialog.setContentView(R.layout.custom_dialog)
-
         dialog.window!!.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT)
         dialog.setCanceledOnTouchOutside(true)
         dialog.setCancelable(true)
         dialog.show()
 
-//        val db = Room.databaseBuilder(
-//            applicationContext,
-//            UserDatabase::class.java,"database"
-//        ).allowMainThreadQueries().build()
-//        db객체 생성
-
         val ok = dialog.findViewById<TextView>(R.id.ok3)
         ok.setOnClickListener {
             dialog.dismiss()
         }
-
-
     }
 
 }
