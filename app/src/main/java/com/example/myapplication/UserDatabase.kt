e0c21979b24ff40673765c9db0fa13227b330418
west <jsistop16@naver.com>
2022-01-05 17:20:46 +0900

    roomDB migration 버전 업그레이드(변경사항 발생시)

--- app/src/main/java/com/example/myapplication/UserDatabase.kt
+++ app/src/main/java/com/example/myapplication/UserDatabase.kt
@@ -2,6 +2,8 @@ package com.example.myapplication
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
 
 @Database(entities = arrayOf(User::class), version = 1)
@@ -9,5 +11,19 @@ abstract class UserDatabase : RoomDatabase() {
 
     abstract fun userDao() : UserDao
 
+    //roomDB migration
+    object MigrateDatabase{
+        //버전별 변경내역 : 버전 1에서 2로 바뀔 때 적용되는 사항
+        val MIGRATE_1_2 = object : Migration(1,2){
+            override fun migrate(database: SupportSQLiteDatabase) {
+                //변경 쿼리(ex. column추가)
+                val alter = "ALTER table users add column age"
+                //쿼리 적용
+                database.execSQL(alter)
+            }
+        }
+        //다음 버전 업데이트 적용되는 사항...
+
+    }
 
 }
